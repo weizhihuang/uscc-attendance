b56d996946982805365a130dedb2620ef43c8b38
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2020-03-06 18:45:31 +0800

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  div.fill-height
+    v-container(fill-height)
+      v-container
+        v-row.text-center
+          v-col
+            h1.display-2.font-weight-bold ようこそ USCC Lab
+        v-row.text-right
+          v-col
+            p.headline {{ now }}
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data: () => ({
+    timer: null,
+    options: {
+      dateStyle: "full",
+      timeStyle: "short",
+      hour12: false
+    },
+    now: ""
+  }),
+  created() {
+    this.timer = setInterval(() => {
+      this.now = new Date().toLocaleString(undefined, this.options);
+    }, 500);
+  },
+  destroyed() {
+    clearInterval(this.timer);
   }
 };
 </script>
