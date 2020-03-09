dbf1ccd4c5b05bd62ea45bb3e4bcf93ab77ed816
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2020-03-09 17:57:11 +0800

    link

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
   v-app
     v-navigation-drawer(v-model="drawer" app)
       v-list(dense)
-        v-list-item(link)
+        v-list-item(to="home")
           v-list-item-action
             v-icon mdi-home
           v-list-item-content
             v-list-item-title 首頁
+        v-list-item(to="members")
+          v-list-item-action
+            v-icon mdi-account
+          v-list-item-content
+            v-list-item-title 實驗室成員
+        v-list-item(to="record")
+          v-list-item-action
+            v-icon mdi-calendar-clock
+          v-list-item-content
+            v-list-item-title 出席記錄
     
     v-app-bar(app color="primary" dark)
       v-app-bar-nav-icon(@click.stop="drawer = !drawer")
