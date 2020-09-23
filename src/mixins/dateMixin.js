ca18342fbb1d059ffea277d768ab52abfedca1c9
WeiZhiHuang <p76084449@gs.ncku.edu.tw>
2020-09-23 08:21:21 +0100

    modify toTimeString

--- src/mixins/dateMixin.js
+++ src/mixins/dateMixin.js
@@ -1,9 +1,13 @@
 export const dateMixin = {
   methods: {
-    toTimeString(t) {
+    toTimeString(t, convertToDays = false) {
+      let d;
+
       t /= 60000;
-      const d = ~~(t / 1440);
-      t %= 1440;
+      if (convertToDays) {
+        d = ~~(t / 1440);
+        t %= 1440;
+      }
       const h = ~~(t / 60);
       const m = ~~(t % 60);
       return `${d ? d + " 天" : ""} ${h} 時 ${m} 分`;
