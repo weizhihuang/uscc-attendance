65b88135757485f24b366d565bc79b5af785109f
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2020-10-24 15:51:53 +0800

    fix v-content padding when print

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,44 @@
 <template lang="pug">
-  v-app
-    v-navigation-drawer(v-model="drawer" app)
-      v-list(dense)
-        v-list-item(to="home")
-          v-list-item-action
-            v-icon mdi-home
-          v-list-item-content
-            v-list-item-title 首頁
-        v-list-item(to="members")
-          v-list-item-action
-            v-icon mdi-account
-          v-list-item-content
-            v-list-item-title 實驗室成員
-        v-list-item(to="records")
-          v-list-item-action
-            v-icon mdi-calendar-clock
-          v-list-item-content
-            v-list-item-title 出席記錄
-    
-    v-app-bar.hidden-print-only(app color="primary" dark)
-      v-app-bar-nav-icon(@click.stop="drawer = !drawer")
-      v-toolbar-title
-        | USCC打卡系統
-        span.subtitle-1.blue-grey--text.text--lighten-4  v{{ version }}
+v-app
+  v-navigation-drawer(v-model="drawer", app)
+    v-list(dense)
+      v-list-item(to="home")
+        v-list-item-action
+          v-icon mdi-home
+        v-list-item-content
+          v-list-item-title 首頁
+      v-list-item(to="members")
+        v-list-item-action
+          v-icon mdi-account
+        v-list-item-content
+          v-list-item-title 實驗室成員
+      v-list-item(to="records")
+        v-list-item-action
+          v-icon mdi-calendar-clock
+        v-list-item-content
+          v-list-item-title 出席記錄
 
-    v-content
-      v-container.fill-height(fluid)
-        router-view(:closeDrawer="() => { drawer = false }")
+  v-app-bar.hidden-print-only(app, color="primary", dark)
+    v-app-bar-nav-icon(@click.stop="drawer = !drawer")
+    v-toolbar-title
+      | USCC打卡系統
+      span.subtitle-1.blue-grey--text.text--lighten-4 v{{ version }}
 
-    v-footer(absolute padless v-show="['Home', 'Members'].includes($route.name)")
-      v-col.text-center
-        p.headline 讀卡機：
-          span.green--text(v-if="readers.length") {{ readers.join(", ") }}
-          span(v-else)
-            span.red--text 未連接
-            v-btn.mb-1.ml-2(outlined small color="warning" @click="reinitNFC") 重啟服務
+  v-content
+    v-container.fill-height(fluid)
+      router-view(:closeDrawer="() => { drawer = false; }")
+
+  v-footer(
+    absolute,
+    padless,
+    v-show="['Home', 'Members'].includes($route.name)"
+  )
+    v-col.text-center
+      p.headline 讀卡機：
+        span.green--text(v-if="readers.length") {{ readers.join(', ') }}
+        span(v-else)
+          span.red--text 未連接
+          v-btn.mb-1.ml-2(outlined, small, color="warning", @click="reinitNFC") 重啟服務
 </template>
 
 <script>
@@ -66,4 +70,8 @@ export default {
 <style lang="sass">
 ::-webkit-scrollbar
   display: none
+
+@media print
+  .v-content
+    padding: 0 !important
 </style>
